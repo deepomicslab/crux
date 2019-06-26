e7e600ea95c817db9ee9eb6931c79330ca079ebe
Collider LI <lhc199652@gmail.com>
2019-06-26 13:36:31 +0800

    Add export for color

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ import { DataLoader } from "./data-loader";
 import * as event from "./event";
 import mouse from "./mouse";
 export * from "./newick";
+export * from "./color";
 
 export { event, mouse, DataLoader };
