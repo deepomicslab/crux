546e259a49a93a5d5004ab8e7dc51f8f4c58c925
Collider LI <lhc199652@gmail.com>
2020-12-18 17:49:37 +0800

    Update sample tree

--- demo/packages/tree/index.ts
+++ demo/packages/tree/index.ts
@@ -8,31 +8,66 @@ import sunburstData from "../../tree-data/sunburst-data";
 const template = `//bvt
 svg {
     height = 800
-    SunburstTree {
-        data = data;
-        :partition({ leaf, color }) {
-            Arc {
-                x1 = leaf.x0; x2 = leaf.x1;
-                r1 = leaf.y0; r2 = leaf.y1;
-                fill = color
-                pad = 0;
-            }
-        }
-        :overlay({ leaf }) {
-            Text.centered {
-                text = leaf.data.name
-                x = (leaf.x0 + leaf.x1) / 2
-                y = (leaf.y0 + leaf.y1) / 2
+    Tree {
+        width = 800; height = 800
+        isCluster = true
+        direction = "radical"
+        data = data
+        leafSize = 100
+        activeLink.stroke = "#66ccff"
+        branchShouldStayOnTop = linkOnTop
+        :link ({ link, pos, tree }) {
+            Path {
+                @let onTop = linkOnTop(link)
+                stroke = onTop ? "#0f0" : link.target.data.name.length % 2 === 0 ? "#f00" : "#00f"
+                strokeWidth = onTop ? 4 : 2
+                fill = "none"
+                d = tree.getPath(...pos)
             }
         }
     }
+    // SunburstTree {
+    //     data = data;
+    //     :partition({ leaf, color }) {
+    //         Arc {
+    //             x1 = leaf.x0; x2 = leaf.x1;
+    //             r1 = leaf.y0; r2 = leaf.y1;
+    //             fill = color
+    //             pad = 0;
+    //         }
+    //     }
+    //     :overlay({ leaf }) {
+    //         Text.centered {
+    //             text = leaf.data.name
+    //             x = (leaf.x0 + leaf.x1) / 2
+    //             y = (leaf.y0 + leaf.y1) / 2
+    //         }
+    //     }
+    // }
 }
 `;
 
+const d = parseNewick(newickData.yiqi);
+console.log(d);
+
 const data = {
-    // data: treeAlgo(parseNewick(newickData.tumor) as Node, bg),
-    data: sunburstData,
+    // data: treeAlgo(parseNewick(newickData.yiqi) as Node, bg),
+    data: d,
+    // data: sunburstData,
     bg,
+    linkOnTop(link) {
+        const h = hasChildren(link.target, "Lachnospiraceae bacterium KGMB03038");
+        return h;
+    },
 };
+
+function hasChildren(node, name) {
+    if (node.data.name === name) return true;
+    if (!node.children) return false;
+    for (const c of node.children) {
+        if (hasChildren(c, name)) return true;
+    }
+    return false;
+}
 
 export { template, data };
